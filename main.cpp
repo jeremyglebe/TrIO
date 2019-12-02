f2db24721e38083a92280181bc300368627c8fba
Jeremy D Glebe <jeremyglebe@gmail.com>
2019-12-02 03:29:19 +0000

    identified and commented errors on *nix

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@ int main(){
     print("Testing... Testing...\n");
     print("Goodbye World!\n");
 
-    // plant randomly positioned red X's in the terminal
+    // plant randomly positioned green X's in the terminal
     srand(time(NULL));
     for(int i = 0; i < 9; i++){
-        moveCursor(rand() % 10, rand() % 6 + 3);
+        // x and y are flipped from the documented function...?
+        moveCursor(rand() % 3 + 4, rand() % 10);
         print("&40X");
     }
+
+    // Counting from 1,1. If you enter 0, it becomes 1...?
+    moveCursor(0, 0);
+    print("&30X");
+    moveCursor(1, 1);
+    print("&30X");
 
 }
