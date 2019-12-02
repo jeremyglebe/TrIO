31bdbc4f8de3e4c3c46d437dfd1a80317b99e71d
Jeremy D Glebe <jeremyglebe@gmail.com>
2019-12-02 03:29:42 +0000

    better error comment

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ int main(){
     // plant randomly positioned green X's in the terminal
     srand(time(NULL));
     for(int i = 0; i < 9; i++){
-        // x and y are flipped from the documented function...?
+        // *nix: x and y are flipped from the documented function...?
         moveCursor(rand() % 3 + 4, rand() % 10);
         print("&40X");
     }
 
-    // Counting from 1,1. If you enter 0, it becomes 1...?
+    // *nix: Counting from 1,1. If you enter 0, it becomes 1...?
     moveCursor(0, 0);
     print("&30X");
     moveCursor(1, 1);
